62d8ddec0d38673c1aa6d998a992b88a64f2fe20
michal92299 <chudybyczek2137@gmail.com>
2025-11-15 22:21:53 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo mkdir -p /usr/lib/epic-session
+sudo cp epic-session /usr/lib/epic-session/
+sudo cp epic-session-frontend /usr/lib/epic-session/
+sudo cp config.yaml /usr/lib/epic-session/
+sudo cp epic-session-session /usr/lib/epic-session/
+
+echo "Epic Session zainstalowany! Wybierz 'Epic Session' przy logowaniu."
